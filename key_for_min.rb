b6910a3ed385921e1d27c08783ed632b59c1adaf
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 02:13:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,14 @@
 
 def key_for_min_value(name_hash)
    hash = {:blake => 500, :ashley => 2, :adam => 1}
-   hash.each { |k, v| puts k if v == hash.values.max }
+   hash.key(hash.values.max)
    
 end
 
 def key_for_min_value(name_hash)
    hash = {:blake => 500, :ashley => 2, :adam => 1}
-    hash.each { |k, v| puts k if v == hash.values.min }
+    hash.key(hash.values.min)
   
  end 
  
- hash.key(hash.values.max)
+ 
