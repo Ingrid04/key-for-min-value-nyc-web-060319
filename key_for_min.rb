32293c22e06e1a4c0abdfe78ae31681794d91432
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 02:20:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ end
  
  def key_for_min_value(name_hash)
    
-   hash = {:blake => 10, :ashley => 50, :adam => 17}
+    hash = {:blake => 10, :ashley => 50, :adam => 17}
     hash.key(hash.values.min)
-   end
+    end
 end
